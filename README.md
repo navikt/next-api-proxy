5dbc17a787e0c4d0d352b49001b6c19115ea1d3c
Karl J. Overå <k@rl.run>
2022-09-12 11:24:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# next-api-proxy
+
+A simple library that lets you use nextjs API routes to proxy requests to other applications. Useful if you need to exchange a OBO-token before executing the request.
+
+```bash
+yarn add @navikt/next-api-proxy
+```
+
+```bash
+npm i @navikt/next-api-proxy
+```
+
+### Step 1: TODO
---
+++ src/proxyApiRequest.ts
@@ -0,0 +1,65 @@
+import { Readable } from 'stream';
+import http, { RequestOptions } from 'http';
+import https from 'https';
+
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { copyHeaders, stream2buffer } from './proxyUtils';
+
+interface ProxyApiRouteRequestOptions {
+    hostname: string;
+    https: boolean;
+    req: NextApiRequest;
+    res: NextApiResponse;
+    bearerToken: string;
+}
+
+export async function proxyApiRouteRequest({
+    hostname,
+    req,
+    res,
+    bearerToken,
+    https: useHttps,
+}: ProxyApiRouteRequestOptions): Promise<void> {
+    const requestOptions: RequestOptions = {
+        hostname,
+        port: useHttps ? 443 : 80,
+        path: '/graphql',
+        method: 'POST',
+        headers: {
+            ...copyHeaders(req.headers),
+            Authorization: `Bearer ${bearerToken}`,
+        },
+    };
+
+    const stream = Readable.from(req);
+    const bodyResponse = await stream2buffer(stream);
+    const backendReq = (useHttps ? https : http).request(requestOptions, (proxyRequestResponse) => {
+        if (proxyRequestResponse.statusCode != null) {
+            res.status(proxyRequestResponse.statusCode);
+        }
+        for (const headersKey in proxyRequestResponse.headers) {
+            const header = proxyRequestResponse.headers[headersKey];
+            if (header) {
+                res.setHeader(headersKey, header);
+            }
+        }
+
+        proxyRequestResponse.on('data', (data: unknown) => {
+            res.write(data);
+        });
+        proxyRequestResponse.on('end', () => {
+            res.end();
+        });
+    });
+
+    backendReq.write(bodyResponse);
+    backendReq.end();
+}
+
+export const proxiedApiRouteConfig = {
+    api: {
+        bodyParser: false,
+        externalResolver: true,
+    },
+};
